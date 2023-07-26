b9c994a49a913eacc1afd4107761a8dbb779c671
Prasath1294 <121788560+Prasath1294@users.noreply.github.com>
2023-07-26 17:01:28 +0530

    Add files via upload

---
+++ Univariate.py
@@ -0,0 +1,68 @@
+class Univariate():
+    global descriptive
+    def quanQual(dataset):
+        quan=[]
+        qual=[]
+        for columnName in dataset.columns:
+            #print(columnName)
+            if(dataset[columnName].dtype=='O'):
+                #print("qual")
+                qual.append(columnName)
+            else:
+                #print("quan")
+                quan.append(columnName)
+        return quan,qual
+
+    def freqTable(columnName,dataset):
+        freqTable=pd.DataFrame(columns=["Unique_Values","Frequency","Relative Frequency","Cusum"])
+        freqTable["Unique_Values"]=dataset[columnName].value_counts().index
+        freqTable["Frequency"]=dataset[columnName].value_counts().values
+        freqTable["Relative Frequency"]=(freqTable["Frequency"]/103)
+        freqTable["Cusum"]=freqTable["Relative Frequency"].cumsum()
+        return freqTable
+
+    def Univariate(dataset,quan):
+        import pandas as pd
+        import numpy as np
+        descriptive = pd.DataFrame(index=["Mean","Median","Mode","Q1:25%","Q2:50%",
+                                   "Q3:75%","99%","Q4:100%","IQR","1.5rule","Lesser","Greater","Min","Max"],columns=quan)
+        for columnName in quan:
+            descriptive[columnName]["Mean"]=dataset[columnName].mean()
+            descriptive[columnName]["Median"]=dataset[columnName].median()
+            descriptive[columnName]["Mode"]=dataset[columnName].mode()[0]
+            descriptive[columnName]["Q1:25%"]=dataset.describe()[columnName]["25%"]
+            descriptive[columnName]["Q2:50%"]=dataset.describe()[columnName]["50%"]
+            descriptive[columnName]["Q3:75%"]=dataset.describe()[columnName]["75%"]
+            descriptive[columnName]["99%"]=np.percentile(dataset[columnName],99)
+            descriptive[columnName]["Q4:100%"]=dataset.describe()[columnName]["max"]
+            descriptive[columnName]["IQR"]=descriptive[columnName]["Q3:75%"]-descriptive[columnName]["Q1:25%"]
+            descriptive[columnName]["1.5rule"]=1.5*descriptive[columnName]["IQR"]
+            descriptive[columnName]["Lesser"]=descriptive[columnName]["Q1:25%"]-descriptive[columnName]["1.5rule"]
+            descriptive[columnName]["Greater"]=descriptive[columnName]["Q3:75%"]+descriptive[columnName]["1.5rule"]
+            descriptive[columnName]["Min"]=dataset[columnName].min()
+            descriptive[columnName]["Max"]=dataset[columnName].max()
+            descriptive[columnName]["kurtosis"]=dataset[columnName].kurtosis()
+            descriptive[columnName]["skew"]=dataset[columnName].skew()
+            descriptive[columnName]["Var"]=dataset[columnName].var()
+            descriptive[columnName]["Std"]=dataset[columnName].std()
+        return descriptive
+    
+    
+    def findoutliers(dataset,quan):
+        lesser=[]
+        greater=[]
+        descriptive
+        for columnName in quan:
+            if (descriptive[columnName]["Lesser"]>descriptive[columnName]["Min"]):
+                lesser.append(columnName)
+            if (descriptive[columnName]["Greater"]<descriptive[columnName]["Max"]):
+                greater.append(columnName)
+        return lesser,greater
+    
+    def replacingoutliers(dataset,lesser,greater):
+        descriptive
+        for columnName in lesser:
+            dataset[columnName][dataset[columnName]<descriptive[columnName]["Lesser"]]=descriptive[columnName]["Lesser"]
+        for columnName in greater:
+            dataset[columnName][dataset[columnName]>descriptive[columnName]["Greater"]]=descriptive[columnName]["Greater"]
+        return lesser,greater
